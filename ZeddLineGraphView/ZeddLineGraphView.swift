d73b9d2d9c225c952e6cc4ec3285e91f405da34d
Zedd0202 <ch41150@hanmail.net>
2020-04-23 16:22:11 +0900

    add Animation

--- ZeddLineGraphView/ZeddLineGraphView.swift
+++ ZeddLineGraphView/ZeddLineGraphView.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ZeddLineGraph: UIView {
+class ZeddLineGraphView: UIView {
     
     var values: [CGFloat] = []
     
+    var graphPath: UIBezierPath!
+    var zeroPath: UIBezierPath!
+    
+    let graphLayer = CAShapeLayer()
+
     init(frame: CGRect, values: [CGFloat]) {
         super.init(frame: frame)
         self.values = values
@@ -20,30 +25,42 @@ class ZeddLineGraph: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func draw(_ rect: CGRect) {
         super.draw(rect)
+        self.graphPath = UIBezierPath()
+        self.zeroPath = UIBezierPath()
         
-        let layer = CAShapeLayer()
-        let path = UIBezierPath()
+        self.layer.addSublayer(graphLayer)
 
         let xOffset: CGFloat = self.frame.width / CGFloat(values.count)
         
         var currentX: CGFloat = 0
-        path.move(to: CGPoint(x: currentX, y: self.frame.height))
-    
+        let startPosition = CGPoint(x: currentX, y: self.frame.height)
+        self.graphPath.move(to: startPosition)
+        self.zeroPath.move(to: startPosition)
+        
         for i in 0..<values.count {
             currentX += xOffset
             let newPosition = CGPoint(x: currentX,
                                       y: self.frame.height - self.values[i])
-            path.addLine(to: newPosition)
+            self.graphPath.addLine(to: newPosition)
+            self.zeroPath.addLine(to: CGPoint(x: currentX, y: self.frame.height))
         }
         
-        layer.fillColor = nil
-        layer.strokeColor = UIColor.black.cgColor
-        layer.lineWidth = 2
-        layer.path = path.cgPath
-        self.layer.addSublayer(layer)
- 
+        graphLayer.fillColor = nil
+        graphLayer.strokeColor = UIColor.black.cgColor
+        graphLayer.lineWidth = 2
+        
+        let oldPath = self.zeroPath.cgPath
+        let newPath = self.graphPath.cgPath
+                
+        let animation = CABasicAnimation(keyPath: "path")
+        animation.duration = 1
+        animation.fromValue = oldPath
+        animation.toValue = newPath
+        
+        self.graphLayer.path = newPath
+        self.graphLayer.add(animation, forKey: "path")
     }
 }
